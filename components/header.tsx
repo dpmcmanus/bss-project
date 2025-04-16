917bda5755153978bffc23d088aade216b09956d
Donovan McManus <donovan.mcmanus2020@gmail.com>
2025-04-15 20:29:55 -0400

    Implement a redirecting middleware

--- components/header.tsx
+++ components/header.tsx
@@ -5,6 +5,8 @@ import { AdminNavEntry } from "./AdminNavEntry"
 import { headers } from "next/headers"
 import { auth } from "@/lib/auth"
 
+export const dynamic = 'force-dynamic';
+
 export async function Header() {
 
     const session = await auth.api.getSession({ headers: await headers() });
