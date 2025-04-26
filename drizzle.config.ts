bd4cde37aab595a9ce170ae9e5a2dd81a237664a
Donovan McManus <donovan.mcmanus2020@gmail.com>
2025-04-26 13:25:20 -0400

    Changes to drizzle.config.ts to work locally.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,14 +2,15 @@ import "dotenv/config"
 
 import { defineConfig } from 'drizzle-kit';
 
-const url = process.env.DATABASE_URL
-if (!url)
-  throw new Error(
-    `Connection string to ${process.env.NODE_ENV ? 'Neon' : 'local'} Postgres not found.`
-  );
-export default defineConfig({
-  out: './database/migrations',
-  schema: './database/schema/*',
-  dialect: 'postgresql',
-  dbCredentials: { url: "postgresql://neondb_owner:npg_G8TgWo3AQSVI@ep-nameless-band-a5ss60pt.us-east-2.aws.neon.tech/neondb?sslmode=require" },
-});
+const url =
+  process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : process.env.LOCAL_DATABASE_URL;
+ if (!url)
+   throw new Error(
+     `Connection string to ${process.env.NODE_ENV ? 'Neon' : 'local'} Postgres not found.`
+   );
+ export default defineConfig({
+   out: './database/migrations',
+   schema: './database/schema/*',
+   dialect: 'postgresql',
+   dbCredentials: { url },
+ });
