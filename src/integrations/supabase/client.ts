908ddc8a5a9c5b256bae947253e63a33a6e4c867
Donovan McManus <donovan.mcmanus2020@gmail.com>
2025-04-28 21:19:52 -0400

    Update for correct supabase url

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://zrjcsgkxcawttvamebng.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyamNzZ2t4Y2F3dHR2YW1lYm5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3ODY5NzUsImV4cCI6MjA2MTM2Mjk3NX0.aN6ZVRdputjUei6xoUrq2u8ei2DW1pFg5_UWZxwR5MU";
+const SUPABASE_URL = "https://afqqztcarqhtyijcafvz.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmcXF6dGNhcnFodHlpamNhZnZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4ODQ0MzgsImV4cCI6MjA2MTQ2MDQzOH0.xQM2YxjXmVSJAW_vNotG1f-fzX77XRlO0W5SJeA5PO4";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
